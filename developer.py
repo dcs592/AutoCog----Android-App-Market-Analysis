21bab99327bd49d3314f0fe167e437c483e115cc
Dane Stier <DaneStier@u.northwestern.edu>
2014-05-21 03:01:21 -0500

    Added analysis files

---
+++ developer.py
@@ -0,0 +1,258 @@
+#===============================#
+#==# Import all dependencies #==#
+#===============================#
+import json, os, sys, math, re
+import numpy as np
+from glob import glob
+from operator import itemgetter
+import sqlite3
+from StringIO import StringIO
+from pprint import pprint
+import urllib2
+
+#=================================================#
+#==# Load Application Questionable Permissions #==#
+#=================================================#
+filename = raw_input("Input path to JSON of application TOTAL permissions: ")
+#filename = os.path.expanduser("~/Documents/Northwestern/Spring14/EECS450/allquestdict.json")
+json_data = open(filename)
+quest_perms = json.load(json_data)
+print("\n\nNumber of Apps with Questionable Perm Data: " + str(len(quest_perms)))
+
+#==========================================#
+#==# Load Application Total Permissions #==#
+#==========================================#
+filename = raw_input("Input path to JSON of application QUESTIONABLE permissions: ")
+#filename = os.path.expanduser("~/Documents/Northwestern/Spring14/EECS450/all_permdict.json")
+json_data = open(filename)
+all_perms = json.load(json_data)
+print("Number of Apps with Total Perm Data: " + str(len(all_perms)))
+
+#=============================================#
+#==# Load Database of Application Metadata #==#
+#=============================================#
+filename = os.path.expanduser("autocog2.db")
+db_data = sqlite3.connect(filename)
+c = db_data.cursor()
+
+#==============================================#
+#==# Variables to Store Data During Parsing #==#
+#==============================================#
+devs = dict()
+total_fail1 = 0
+total_fail2 = 0
+
+#===================================#
+#==# Pull Metadata from Database #==#
+#===================================#
+for row in c.execute('SELECT apkname, category, developer, more_from_developer, rating FROM apps'):
+	apkname = row[0]
+	category = row[1]
+	developer = row[2]
+	more_apps = row[3]
+	rating = row[4]
+
+	try: 
+		perms_list = quest_perms[row[0] + '.apk']
+	except:
+		total_fail1+= 1
+		continue
+	try:
+		all_list = all_perms[row[0] + '.apk']
+	except:
+		total_fail2+= 1
+		continue
+
+	#add the developer to the developer dict
+	if not developer in devs:
+		devs[developer] = dict()
+		devs[developer]['count'] = 1
+		devs[developer]['ratings'] = dict()
+		devs[developer]['categories'] = dict()
+		devs[developer]['qperms'] = dict()
+		devs[developer]['all_perms'] = dict()
+		devs[developer]['all_perms_list'] = dict()
+	else :
+		devs[developer]['count']+= 1 #increment the count
+
+	#increment the rating count for a developer's rating
+	if rating in devs[developer]['ratings']:
+		devs[developer]['ratings'][rating]+= 1
+	else:
+		devs[developer]['ratings'][rating] = 1
+	#increment the category count for the developer's category
+	if category in devs[developer]['categories']:
+		devs[developer]['categories'][category]+= 1
+	else:
+		devs[developer]['categories'][category] = 1
+	#increment the number of times each questionable permission was used by the developer
+	for p in perms_list:
+		if p in devs[developer]['qperms']:
+			devs[developer]['qperms'][p]+= 1
+		else:
+			devs[developer]['qperms'][p] = 1
+	#increment the number of times each permission was used by the developer
+	for p in all_list:
+		if p in devs[developer]['all_perms']:
+			devs[developer]['all_perms'][p]+= 1
+		else:
+			devs[developer]['all_perms'][p] = 1
+	#increment the number of times the same permission set was used by the developer in all apps
+	plist = ""
+	for item in all_list:
+		plist+= item + '.'
+	if plist in devs[developer]['all_perms_list']:
+		devs[developer]['all_perms_list'][plist]+= 1
+	else:
+		devs[developer]['all_perms_list'][plist] = 1
+
+#==============================================#
+#==# Variables to Store Data During Parsing #==#
+#==============================================#
+i = 0
+num = len(devs)
+num_mult = 0
+match_num = 0
+close_match = 0
+most_perms = dict()
+mperms = 0
+all_same_perms = 0
+nearly_same_perms = 0
+same_categories = 0
+same_ratings = 0
+total_matching_perms = 0
+
+#iterate through the dict of developers
+for d in devs:
+	#narrow the scope to developers with more than one app in the database
+	if devs[d]['count'] > 1:
+		num_mult+= 1 #increment the number of developers being parsed
+		#if the developer used questionable permissions
+		if devs[d]['qperms']:
+			#get the highest instance of questionable permissions
+			max_perm = max(devs[d]['qperms'], key=devs[d]['qperms'].get)
+			#if the number of q_perms == number of apps created
+			if devs[d]['count']==devs[d]['qperms'][max_perm]:
+				match_num+= 1 #increment the number of developers using q_perms in every app
+			#if the number of q_perms == number of apps created - 1
+			elif devs[d]['count']-1==devs[d]['qperms'][max_perm]:
+				close_match+= 1 #increment the number of developers using q_perms in all but one app
+			#if the developers q_perm was the most requested q_perm (in all applications created) in database
+			if max_perm in most_perms:
+				most_perms[max_perm]+= 1
+			else:
+				most_perms[max_perm] = 1
+			#if the most common q_perm was used at all
+			if 'WRITE_EXTERNAL_STORAGE' in devs[d]['qperms']:
+				mperms+= 1
+		#if the developer didn't use questionable permissions
+		else:
+			if devs[d]['count'] == 0:
+				match_num+= 1
+		#if the developer used permissions
+		if devs[d]['all_perms']:
+			max_a_perm = max(devs[d]['all_perms'], key=devs[d]['all_perms'].get)
+			#if the developer used the same permission in all applications
+			if devs[d]['count']==devs[d]['all_perms'][max_a_perm]:
+				total_matching_perms+=1
+		#if the developer used any set of permissions in all apps
+		if devs[d]['all_perms_list']:
+			#get the most commonly used set of permissions
+			max_same_perms = max(devs[d]['all_perms_list'], key=devs[d]['all_perms_list'].get)
+			#if the developer used this set of permissions in all apps
+			if devs[d]['count']==devs[d]['all_perms_list'][max_same_perms]:
+				all_same_perms+= 1 #increment
+			#if the developer used this set of permissions in all but one app
+			elif devs[d]['count']==devs[d]['all_perms_list'][max_same_perms]+1:
+				nearly_same_perms+= 1 #increment
+		#if the developer has listed categories
+		if devs[d]['categories']:
+			#get the most commonly used category
+			max_same_cat = max(devs[d]['categories'], key=devs[d]['categories'].get)
+			#if the developer created apps all in the same category
+			if devs[d]['categories'][max_same_cat]==devs[d]['count']:
+				same_categories+= 1 #increment
+		#if the developer has ratings listed
+		if devs[d]['ratings']:
+			#get the most common rating
+			max_rating = max(devs[d]['ratings'], key=devs[d]['ratings'].get)
+			checker = True
+			#check if all of the developer's apps have a rating in a .2 bound of the most common rating
+			for r in devs[d]['ratings']:
+				if abs(float(max_rating)-float(r))>.2:
+					checker = False
+					break
+			if checker==True:
+				same_ratings+= 1 #increment the number of developers with this narrow span of ratings
+
+print("\n\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("Number of Devs".center(38), #number of developers
+		str(num).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("Dev's w/ Multiple Apps".center(38), #number of developers with multiple apps in database
+		str(num_mult).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("Matching # Perms".center(38), #number of developers using the same q_perm in every app
+		str(match_num).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("-1 Matching Perms".center(38), #number of developers using same q_perm in all but one app
+		str(close_match).center(35)))
+print("\t+========================================+=====================================+")
+print("\t+========================================+=====================================+")
+print("\t| %38s | %35s | " % 
+	("All Same Perms".center(38), #number of developers using the same set of permissions in every app
+		str(all_same_perms).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("-1 Same Perms".center(38), #number of developers using the same set of permissions in all but one app
+		str(nearly_same_perms).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("Single Category".center(38), #number of developers creating all apps of the same category
+		str(same_categories).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("Same Rating".center(38), #number of developers creating apps with ratings within a .4 span
+		str(same_ratings).center(35)))
+print("\t+========================================+=====================================+")
+print("\t+========================================+=====================================+")
+print("\t| %38s | %35s | " % 
+	("Same Perm in All Apps".center(38), #number of developers using the same permissions in all apps
+		str(total_matching_perms).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+max_perm = max(most_perms, key=most_perms.get)
+print("\t| %38s | %35s | " % 
+	("Most Requested Permission".center(38), #most commonly requested q_perm
+		(max_perm+" ("+str(most_perms[max_perm])+")").center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+print("\t| %38s | %35s | " % 
+	("Total Using Most Requested".center(38), #number of developers using the most common q_perm
+		str(mperms).center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+del most_perms[max_perm]
+max_perm = max(most_perms, key=most_perms.get)
+print("\t| %38s | %35s | " % 
+	("2nd Most Requested Perm".center(38), #second most commonly requested q_perm
+		(max_perm+" ("+str(most_perms[max_perm])+")").center(35)))
+print("\t+----------------------------------------+-------------------------------------+")
+del most_perms[max_perm]
+max_perm = max(most_perms, key=most_perms.get)
+print("\t| %38s | %35s | " % 
+	("3rd Most Requested Perm".center(38), #third most commonly requested q_perm
+		(max_perm+" ("+str(most_perms[max_perm])+")").center(35)))
+print("\t+----------------------------------------+-------------------------------------+\n\n")
+
+
+
+
+
+
+
+
+
+
+
---
+++ ratings.py
@@ -0,0 +1,204 @@
+#===============================#
+#==# Import all dependencies #==#
+#===============================#
+import json, os, sys, math, operator
+import numpy as np
+import sqlite3
+from StringIO import StringIO
+from pprint import pprint
+import urllib2
+
+#=================================================#
+#==# Load Application Questionable Permissions #==#
+#=================================================#
+filename = raw_input("Input path to JSON of application TOTAL permissions: ")
+#filename = os.path.expanduser("~/Documents/Northwestern/Spring14/EECS450/allquestdict.json")
+json_data = open(filename)
+quest_perms = json.load(json_data)
+print("\n\nNumber of Apps with Questionable Perm Data: " + str(len(quest_perms)))
+
+#==========================================#
+#==# Load Application Total Permissions #==#
+#==========================================#
+filename = raw_input("Input path to JSON of application QUESTIONABLE permissions: ")
+#filename = os.path.expanduser("~/Documents/Northwestern/Spring14/EECS450/all_permdict.json")
+json_data = open(filename)
+data = json.load(json_data)
+print("Number of Apps with Total Perm Data: " + str(len(data)))
+
+#=============================================#
+#==# Load Database of Application Metadata #==#
+#=============================================#
+filename = os.path.expanduser("autocog2.db")
+db_data = sqlite3.connect(filename)
+c = db_data.cursor()
+
+#==============================================#
+#==# Variables to Store Data During Parsing #==#
+#==============================================#
+totalapks = 0
+totalfailed1 = 0
+totalfailed2 = 0
+ratings = dict()
+categories = dict()
+installs = dict()
+price = dict()
+votes = dict()
+
+#===================================#
+#==# Pull Metadata from Database #==#
+#===================================#
+for row in c.execute('SELECT apkname, rating, category, installs, price, votes FROM apps'):
+	totalapks+= 1
+	try: #get the questionable permissions from the JSON
+		Q_perms = quest_perms[row[0] + '.apk']
+	except: 
+		totalfailed1+= 1
+		continue
+
+	try: #get the total permissions from the JSON
+		T_perms = data[row[0] + '.apk']
+	except:
+		totalfailed2+= 1
+		continue
+
+	len_Q_perms = len(Q_perms) #length of the questionable permissions
+	len_T_perms = len(T_perms) #length of the total permissions
+
+	#if no rating, assign it to a 0.0 rating
+	if row[1]=='':
+		r = '0.0'
+	else:
+		r = row[1]
+	#add the number of q_perms and t_perms to the relevant rating
+	if r in ratings:
+		ratings[r][0].append(len_Q_perms)
+		ratings[r][1].append(len_T_perms)
+	else:
+		ratings[r] = [[len_Q_perms],[len_T_perms]]
+	#add the number of q_perms and t_perms to the relevant category
+	if row[2] in categories:
+		categories[row[2]].append(len_Q_perms)
+	else:
+		categories[row[2]] = [len_Q_perms]
+	#average the number of installations
+	if '-' in row[3]:
+		a, b = row[3].split('-')
+		while ',' in a:
+			a = a.replace(',', '')
+		while ',' in b:
+			b = b.replace(',', '')
+		a, b = int(a), int(b)
+		average = (a+b)/2
+		#add the average number of installations to the dict
+		if row[1] in installs:
+			installs[row[1]].append(average)
+		else:
+			installs[row[1]] = [average]
+	#add the price to the relevant rating
+	if '$' in row[4]:
+		cost = float(row[4][1:])
+	else: 
+		cost = float(row[4])
+	if row[1] in price:
+		price[row[1]].append(cost)
+	else:
+		price[row[1]] = [cost]
+
+	#if no vote is recorded, change it to 0
+	if (row[5]==''):
+		vote = 0
+	else:
+		vote = int(row[5])
+	#add the vote to the relevant rating
+	if row[1] in votes:
+		votes[row[1]].append(vote)
+	else:
+		votes[row[1]] = [vote]
+
+print("Total Apps: " + str(totalapks))
+print("Failed Questionable Perm Queries: " + str(totalfailed1))
+print("Failed Total Perm Queries: " + str(totalfailed2))
+
+#====================================#
+#==# Print Data Regarding Ratings #==#
+#====================================#
+print("\n\nQUESTIONABLE PERMISSIONS AND INSTALLATIONS")
+print("+--------+-----------+---------+--------------+-----------+--------------+---------+-------+-------------+")
+print("| %6s | %9s | %7s | %12s | %9s | %12s | %7s | %5s | %11s |  " % 
+	("Rating".center(6), #rating number
+		"# Q-Perms".center(9), #average number of questionable permissions
+		"# Perms".center(7), #average number of total permissions
+		"Ave Installs".center(12), #average number of installations
+		"Med Perms".center(9), #median number of questionable permissions
+		"Med Installs".center(12), #median number of installations
+		"Total #".center(7), #total number of available apps at this rating
+		"Price".center(5), #average price per app at this rating
+		"Votes".center(11))) #average number of votes for apps at this rating
+print("+========+===========+=========+==============+===========+==============+=========+=======+=============+")
+keys = ratings.keys()
+keys.sort()
+
+#iterate through the values in the dicts
+for k in keys:
+	#sort and calculate averages and median values
+	ratings[k][0] = sorted(ratings[k][0])
+	ratings[k][1] = sorted(ratings[k][1])
+	installs[k] = sorted(installs[k])
+	total = len(ratings[k][1])
+	perm_ave = np.average(ratings[k][0])
+	allp_ave = np.average(ratings[k][1])
+	inst_ave = np.average(installs[k])
+	perm_med = np.median(ratings[k])
+	inst_med = np.median(ratings[k])
+	ave_price = np.average(price[k])
+	ave_votes = np.average(votes[k])
+
+	print("| %6s | %9s | %7s | %12s | %9s | %12s | %7s | %5s | %11s | " % 
+		(k.center(6), 
+			("%.5f" % round(perm_ave,5)).center(9), 
+			("%.4f" % round(allp_ave, 4)).center(7), 
+			("%.4f" % round(inst_ave, 4)).center(12), 
+			("%.5f" % round(perm_med, 5)).center(9), 
+			("%.5f" % round(inst_med, 5)).center(12), 
+			str(total).center(7), 
+			("%.2f" % round(ave_price, 2)).center(5), 
+			("%.5f" % round(ave_votes, 5)).center(11)))
+	print("+--------+-----------+---------+--------------+-----------+--------------+---------+-------+-------------+")
+
+print("\n\n")
+
+#=======================================#
+#==# Print Data Regarding Categories #==#
+#=======================================#
+print("QUESTIONABLE PERMISSIONS BY CATEGORY")
+print("+-------------------+-------------+-------------+")
+print("| %17s | %11s | %11s | " % 
+	("Category".center(17), #app category
+		"Ave # Perms".center(11), #average number of questionable permissions
+		"Med # Perms".center(11))) #median number of questionable permissions
+print("+===================+=============+=============+")
+
+keys = categories.keys()
+#iterate through the values in the dicts
+for k in keys:
+	#sort and calculate averages and median values
+	temp = sorted(categories[k])
+	ave = np.average(temp)
+	med = np.median(temp)
+	categories[k] = [ave, med]
+
+categories = sorted(categories.iteritems(), key=operator.itemgetter(1))
+for key, value in categories:
+	print("| %17s | %11s | %11s | " % 
+		(key.center(17), 
+			("%.5f" % round(value[0], 5)).center(11), 
+			str(value[1]).center(11)))
+	print("+-------------------+-------------+-------------+")
+print("\n\n")
+
+
+
+
+
+
